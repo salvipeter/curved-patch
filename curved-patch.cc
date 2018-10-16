56ee16a73861f6619f52baf57f97e10b851a4b2b
Peter Salvi <vukung@yahoo.com>
2018-10-16 14:56:44 +0200

    Boundary fix; optional GC output

--- curved-patch.cc
+++ curved-patch.cc
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <iostream>
 
+#include <surface-generalized-coons.hh>
+
 #include "curved-gc.hh"
 
 CurveVector readLOP(std::string filename) {
@@ -30,9 +32,45 @@ CurveVector readLOP(std::string filename) {
   return result;
 }
 
+void fixMesh(TriMesh &mesh, const CurveVector &cv, size_t resolution) {
+  size_t index = 0;
+  for (const auto &c : cv)
+    for (size_t i = 0; i < resolution; ++i) {
+      double u = (double)i / resolution;
+      mesh[index++] = c->eval(u);
+    }
+}
+
+void surfaceTest(std::string name, std::shared_ptr<Surface> &&surf, const CurveVector &cv,
+                 std::string filename, size_t resolution, bool fix_mesh = false) {
+  std::cout << name << ":" << std::endl;
+  std::chrono::steady_clock::time_point begin, end;
+
+  begin = std::chrono::steady_clock::now();
+  surf->setCurves(cv);
+  surf->setupLoop();
+  surf->update();
+  end = std::chrono::steady_clock::now();
+  std::cout << "  Setup time: "
+            << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count()
+            << "ms" << std::endl;
+
+  begin = std::chrono::steady_clock::now();
+  auto mesh = surf->eval(resolution);
+  end = std::chrono::steady_clock::now();
+  std::cout << "  Evaluation time: "
+            << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count()
+            << "ms" << std::endl;
+
+  if (fix_mesh)
+    fixMesh(mesh, cv, resolution); // computes exact boundaries
+  mesh.writeOBJ(filename);
+}
+
 int main(int argc, char **argv) {
-  if (argc < 3 || argc > 4) {
-    std::cerr << "Usage: " << argv[0] << " infile.lop outfile.obj [resolution]" << std::endl;
+  if (argc < 3 || argc > 5) {
+    std::cerr << "Usage: " << argv[0]
+              << " infile.lop outfile.obj [resolution] [alternative.obj]" << std::endl;
     return 1;
   }
   CurveVector cv = readLOP(argv[1]);
@@ -41,27 +79,13 @@ int main(int argc, char **argv) {
     return 2;
   }
   size_t resolution = 30;
-  if (argc == 4)
+  if (argc >= 4)
     resolution = std::atoi(argv[3]);
 
-  std::chrono::steady_clock::time_point begin, end;
-
-  begin = std::chrono::steady_clock::now();
-  CurvedGC surf;
-  surf.setCurves(cv);
-  surf.setupLoop();
-  surf.update();
-  end = std::chrono::steady_clock::now();
-  std::cout << "Setup time: "
-            << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count()
-            << "ms" << std::endl;
-
-  begin = std::chrono::steady_clock::now();
-  surf.eval(resolution).writeOBJ(argv[2]);
-  end = std::chrono::steady_clock::now();
-  std::cout << "Evaluation time: "
-            << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count()
-            << "ms" << std::endl;
+  surfaceTest("CGC", std::make_shared<CurvedGC>(), cv, argv[2], resolution, true);
+  if (argc >= 5)
+    surfaceTest("GC", std::make_shared<Transfinite::SurfaceGeneralizedCoons>(),
+                cv, argv[4], resolution);
 
   return 0;
 }
